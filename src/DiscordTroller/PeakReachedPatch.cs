c7ae425f697bac5fd4d75e64bde463069254cf0c
snwappy <snappypupper@gmail.com>
2026-01-01 01:12:41 +0700

    Height update

--- src/DiscordTroller/PeakReachedPatch.cs
+++ src/DiscordTroller/PeakReachedPatch.cs
@@ -25,11 +25,13 @@ class Patch_PeakReached
         string name = Player.localPlayer.photonView.Owner.NickName;
 
         // Y axis height meters tracking from the game code
-        float height = 0f;
-        if (__instance.refs.stats != null)
-        {
-            height = __instance.refs.stats.heightInMeters;
-        }
+        // this doesn't work at Peak?
+
+        //float height = 0f;
+        //if (__instance.refs.stats != null)
+        //{
+        //    height = __instance.refs.stats.heightInMeters;
+        //}
 
         // Webhook message constructor
         string msg = MessageFormatter.Format(Plugin.TplPeak.Value, new Dictionary<string, string>
@@ -37,7 +39,7 @@ class Patch_PeakReached
             ["player"] = name,
             ["datetime"] = localDateTime,
             ["runtime"] = RunTimeHelper.FormatTime(time),
-            ["height"] = height.ToString("0.0"),
+            ["height"] = "So peaked.",
             ["biome"] = GameStateHelper.GetReadableState(),
             ["room"] = GameStateHelper.GetRoomSize(),
             ["mode"] = GameStateHelper.GetGameMode(),
